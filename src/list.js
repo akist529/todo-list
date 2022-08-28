a4046e0bfd5505925b0dbb7ec12b3721d2392ef9
Alex Kist <akist0707@gmail.com>
2022-08-28 09:51:27 -0500

    Add animation upon page load

--- src/list.js
+++ src/list.js
@@ -1,8 +1,11 @@
 export default function List() {
+    const listWrapper = document.createElement("div");
+    listWrapper.id = "list-wrapper";
     const list = document.createElement("div");
     list.id = "list";
     const listTitle = document.createElement("h1");
     listTitle.textContent = "LIST";
     list.appendChild(listTitle);
-    document.getElementById("content").appendChild(list);
+    listWrapper.appendChild(list);
+    document.getElementById("content").appendChild(listWrapper);
 }
