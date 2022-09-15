28c48c7dc7c683ef5120b875fe9b4f8d60abd4b4
Alex Kist <akist0707@gmail.com>
2022-09-14 23:53:18 -0500

    Further develop sidebar buttons and implement grid layout

--- src/sidebar.js
+++ src/sidebar.js
@@ -2,55 +2,76 @@ export default function Sidebar() {
     const sidebar = document.createElement("div");
     sidebar.id = "sidebar";
 
+
     const inbox = document.createElement("button");
-    inbox.className = "sidebar-line";
+    inbox.setAttribute("id", "sidebar-inbox");
+    inbox.setAttribute("class", "sidebar-line");
+
     const inboxPic = document.createElement("img");
     inboxPic.setAttribute("src", "../src/images/timeline.png");
     inbox.appendChild(inboxPic);
+
     const inboxText = document.createElement("h3");
     inboxText.textContent = "Inbox";
     inbox.appendChild(inboxText);
     sidebar.appendChild(inbox);
 
+
     const today = document.createElement("button");
-    today.className = "sidebar-line";
+    today.setAttribute("id", "sidebar-today");
+    today.setAttribute("class", "sidebar-line");
+
     const todayPic = document.createElement("img");
     todayPic.setAttribute("src", "../src/images/today.png");
     today.appendChild(todayPic);
+
     const todayText = document.createElement("h3");
     todayText.textContent = "Today";
     today.appendChild(todayText);
     sidebar.appendChild(today);
 
+
     const calendar = document.createElement("button");
-    calendar.className = "sidebar-line";
+    calendar.setAttribute("id", "sidebar-calendar");
+    calendar.setAttribute("class", "sidebar-line");
+
     const calendarPic = document.createElement("img");
     calendarPic.setAttribute("src", "../src/images/calendar.png");
     calendar.appendChild(calendarPic);
+
     const calendarText = document.createElement("h3");
     calendarText.textContent = "Calendar";
     calendar.appendChild(calendarText);
     sidebar.appendChild(calendar);
 
+
     const projects = document.createElement("button");
-    projects.className = "sidebar-line";
+    projects.setAttribute("id", "sidebar-projects");
+    projects.setAttribute("class", "sidebar-line");
+
     const projectsPic = document.createElement("img");
     projectsPic.setAttribute("src", "../src/images/chevron_right.png");
     projects.appendChild(projectsPic);
+
     const projectsText = document.createElement("h3");
     projectsText.textContent = "Projects";
     projects.appendChild(projectsText);
     sidebar.appendChild(projects);
 
+
     const addProj = document.createElement("button");
-    addProj.className = "sidebar-line";
+    addProj.setAttribute("id", "sidebar-add");
+    addProj.setAttribute("class", "sidebar-line");
+
     const addProjPic = document.createElement("img");
     addProjPic.setAttribute("src", "../src/images/folder.png");
     addProj.appendChild(addProjPic);
+
     const addProjText = document.createElement("h3");
     addProjText.textContent = "Add Project";
     addProj.appendChild(addProjText);
     sidebar.appendChild(addProj);
 
+    
     document.getElementById("content").appendChild(sidebar);
 }
